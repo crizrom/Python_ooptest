8878759064b7c11c8ea1655ba4c2f5c086ab4aa6
crizrom <99371631+crizrom@users.noreply.github.com>
2023-09-05 13:47:16 -0600

    New test files added

---
+++ class.py
@@ -0,0 +1,9 @@
+#class
+
+class Car:
+    brand = ""
+    model = 2004
+    plate = ""
+
+taxi = Car()
+print(taxi.model)
---
+++ objects_and_classes.py
@@ -0,0 +1,11 @@
+#Clases y objetos II
+
+class Players_A:
+    p1 = 'messi'
+    p2 = 'c.ronaldo'
+
+class Players_B:
+    p3 = 'marcelo'
+    p1 = 'falcao'
+
+print(Players_B.p1)
